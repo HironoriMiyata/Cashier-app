bc6f700057a50ac889c71dfef01661b9bf6d1231
miyatahironori <b1633102@planet.kanazawa-it.ac.jp>
2018-05-21 11:22:47 +0900

    初期画面から計算部分への移動

--- app/src/main/java/kies/cashier/CashierActivity.kt
+++ app/src/main/java/kies/cashier/CashierActivity.kt
@@ -1,7 +1,11 @@
 package kies.cashier
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class CashierActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cashier)
+    }
 }

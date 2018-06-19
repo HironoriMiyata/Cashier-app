fa01d8cafe0536c64e8dcda1b3faac2379491be9
miyatahironori <b1633102@planet.kanazawa-it.ac.jp>
2018-06-19 22:39:58 +0900

    idの検索

--- app/src/main/java/kies/cashier/LoadProductID.kt
+++ app/src/main/java/kies/cashier/LoadProductID.kt
@@ -7,17 +7,24 @@ import io.realm.RealmResults
 lateinit var realm: Realm
 
 class  LoadProductID(){
-    fun  loadProductID():Int{
+    fun  loadProductID():List<Int>{
         realm = Realm.getDefaultInstance()
-        var id = 0
+
         val  productID = realm.where(Product::class.java)
                 .findAll()
                 .map { it.productId }
         productID.sorted()
+
+        realm.close()
+        return productID
+    }
+
+    fun findLoadProductID():Int{
+        var id = 0
+        var productID  = loadProductID()
         while (productID[id] ==id ){
             id++
         }
-        realm.close()
         return id
     }
 }

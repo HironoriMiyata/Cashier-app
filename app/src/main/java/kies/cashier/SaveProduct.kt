f70ff8ddc18a11f29fc953a80e7006c64031b708
HiroMiyata <b1633102@planet.kanazawa-it.ac.jp>
2018-06-19 16:07:28 +0900

    Realmの追加

--- app/src/main/java/kies/cashier/SaveProduct.kt
+++ app/src/main/java/kies/cashier/SaveProduct.kt
@@ -1,2 +1,16 @@
 package kies.cashier
 
+import android.app.Application
+import io.realm.Realm
+
+class  SaveProduct(){
+    lateinit var realm: Realm
+     fun saveProduct() {
+        realm = Realm.getDefaultInstance()
+
+        realm.beginTransaction()
+        //ここに追加や更新の入れる
+        
+        realm.commitTransaction()
+    }
+}

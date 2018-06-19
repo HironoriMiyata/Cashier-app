a0f712f4eaf72e41c0edd533f3dacedd3c69cb9d
miyatahironori <b1633102@planet.kanazawa-it.ac.jp>
2018-06-19 22:45:06 +0900

    idの検索

--- app/src/main/java/kies/cashier/SaveProduct.kt
+++ app/src/main/java/kies/cashier/SaveProduct.kt
@@ -6,10 +6,11 @@ import io.realm.Realm
 class  SaveProduct(){
     lateinit var realm: Realm
      fun saveProduct(productName:String,productValue: Int ) {
-        realm = Realm.getDefaultInstance()
-        var id = LoadProductID()
-        realm.beginTransaction()
-        //ここに追加や更新の入れる
+         val loadId = LoadProductID()
+         realm = Realm.getDefaultInstance()
+         var id = loadId.findLoadProductID()
+         realm.beginTransaction()
+         //ここに追加や更新の入れる
          val product = realm.createObject(Product::class.java,id)
          product.productName = productName
          product.productValue = productValue

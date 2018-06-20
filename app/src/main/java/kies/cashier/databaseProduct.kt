b9e4561d8a28238914fc5ea8cf30ea39be9ada92
HiroMiyata <b1633102@planet.kanazawa-it.ac.jp>
2018-06-20 22:33:34 +0900

    商品名リスト

--- app/src/main/java/kies/cashier/databaseProduct.kt
+++ app/src/main/java/kies/cashier/databaseProduct.kt
@@ -5,13 +5,13 @@ import kies.cashier.Model.DB.LoadProductName
 
 lateinit var  oldProductList : List<String>
 
-fun startDatabaseProduct():List<String>{
+fun startDatabaseProduct():MutableList<String>{
     val loadProdcutName = LoadProductName()
     var productNameList = mutableListOf<String>()
     oldProductList = loadProdcutName.loadProductNmae()
     oldProductList = productNameList
     productNameList.add("+")
 
-    return oldProductList
+    return productNameList
 }
 

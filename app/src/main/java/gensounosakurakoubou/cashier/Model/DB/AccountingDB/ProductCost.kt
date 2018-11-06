9fde7b4cf2f7acfd6cc672e60d349c6ed8178b74
宮田博則 <b1633102@planet.kanazawa-it.ac.jp>
2018-11-06 14:58:46 +0900

    新しいデータベースの追加

---
+++ app/src/main/java/gensounosakurakoubou/cashier/Model/DB/AccountingDB/ProductCost.kt
@@ -0,0 +1,14 @@
+package gensounosakurakoubou.cashier.Model.DB.AccountingDB
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.Required
+
+open class ProductCost : RealmObject(){
+    @PrimaryKey
+    var productId: Int = 0
+    @Required
+    var productName: String = ""
+    var cost: Int = 0
+    var productCount: Int = 0
+}

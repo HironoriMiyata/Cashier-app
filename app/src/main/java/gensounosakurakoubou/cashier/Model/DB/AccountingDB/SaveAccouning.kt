6f6ae51415d66ce0ec48635189ba0b8039245e47
宮田博則 <b1633102@planet.kanazawa-it.ac.jp>
2018-11-09 22:04:22 +0900

    日付の取得

--- app/src/main/java/gensounosakurakoubou/cashier/Model/DB/AccountingDB/SaveAccouning.kt
+++ app/src/main/java/gensounosakurakoubou/cashier/Model/DB/AccountingDB/SaveAccouning.kt
@@ -1,19 +1,23 @@
 package gensounosakurakoubou.cashier.Model.DB.AccountingDB
 
+import android.text.format.DateFormat
 import gensounosakurakoubou.cashier.Model.DB.Accounting
 import io.realm.Realm
 import java.util.*
 
-class SaveAccouning{
-    fun saveAccouning(){
+class SaveAccouning {
+    fun saveAccouning() {
         val loadId = LoadAccouningID()
         val realm = Realm.getDefaultInstance()
-        var id = loadId.findLoadAccouningID()
+        val id = loadId.findLoadAccouningID()
+        val getToday = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"), Locale.JAPAN)
+        val today = getToday.time
+
         //計算のためにコストとセールは初期値を０にすること
         realm.beginTransaction()
         val accouning = realm.createObject(Accounting::class.java, id)
         accouning.accountingId = id
-       // accouning.accountingDay = Calendar.getInstance()
+        accouning.accountingDay =  DateFormat.format("yyyy/MM/dd", today).toString()
         accouning.cost = 0
         accouning.sales = 0
         realm.commitTransaction()

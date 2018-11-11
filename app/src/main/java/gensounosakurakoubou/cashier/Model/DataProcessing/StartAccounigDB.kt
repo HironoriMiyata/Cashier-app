fa042a3be7f29f1d8c35c592fa9c200d7a9fd5d2
宮田博則 <b1633102@planet.kanazawa-it.ac.jp>
2018-11-11 19:02:33 +0900

    売り上げデータを作成する機能の追加

--- app/src/main/java/gensounosakurakoubou/cashier/Model/DataProcessing/StartAccounigDB.kt
+++ app/src/main/java/gensounosakurakoubou/cashier/Model/DataProcessing/StartAccounigDB.kt
@@ -1,10 +1,54 @@
 package gensounosakurakoubou.cashier.Model.DataProcessing
 
 import gensounosakurakoubou.cashier.Model.DB.AccountingDB.SaveAccouning
+import java.util.*
 
-class StartAccounigDB{
-    fun startAccouningDB(){
+class StartAccounigDB {
+    fun startAccouningDB() {
         val saveAccouning = SaveAccouning()
-        saveAccouning.saveAccouning()
+        val getToday = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"), Locale.JAPAN)
+        val nowTime = getToday.get(Calendar.HOUR_OF_DAY)
+        var setTime = 0
+        //何もレコードがないときはすぐにレコードを作成する
+        if(checkDate()){
+            saveAccouning.saveAccouning()
+        }
+        //データが本当に24時間表記か調べて24時間表記に直す
+        setTime = checkTime(setTime)
+        //利用者が決めた時間でレコードの更新が必要か確認する
+        if (nowTime > setTime && setTime < 25) {
+            if (checkDay()) {
+                saveAccouning.saveAccouning()
+            }
+        } else {
+            if (checkOverTime()) {
+                saveAccouning.saveAccouning()
+            }
+        }
+    }
+
+    private  fun checkDate():Boolean{
+        return true
+    }
+
+    private fun checkTime(setTime: Int): Int {
+        var checkedHour = setTime
+        if (setTime > 24) {
+            checkedHour = setTime % 24
+        } else if (setTime < 0) {
+            checkedHour = setTime * (-1)
+            return checkTime(checkedHour)
+        }
+        return checkedHour
+    }
+
+    private fun checkDay(): Boolean {
+
+        return true
+    }
+
+    private fun checkOverTime(): Boolean {
+
+        return true
     }
 }
